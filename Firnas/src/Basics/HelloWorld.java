2b47361a4dc048c9fa7f1755550776845bd50e7e
musaa <musaa@LAPTOP-5C7KLDKA>
2022-02-07 09:23:50 +0530

    Update Hello World

--- Firnas/src/Basics/HelloWorld.java
+++ Firnas/src/Basics/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//Output Statement
 		System.out.println("Hello World!");
+		// Type "main" and press "ctrl + space bar"
+		// Type "sysout" and press "ctrl + space bar"
 	}
 }
