304e0e0851f3df7cc830522868346a5e6465706f
musaa <musaa@LAPTOP-5C7KLDKA>
2022-02-07 19:15:18 +0530

    Getting User Input

---
+++ Firnas/src/Basics/GettingInput.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class GettingInput {
+	public static void main(String[] args) {
+		System.out.println("Getting Input from user");
+		int age;
+		//--For getting input you should import scanner
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your age : ");
+		age = scan.nextInt();
+		
+		System.out.println("Your age after 10 years : "+(age + 10));
+	}
+}
