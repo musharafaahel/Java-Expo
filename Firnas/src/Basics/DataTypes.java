a7068627256bc18a21a207fc7ffe42de7b6b0252
musharafaahel <musaa@LAPTOP-5C7KLDKA>
2022-02-07 09:53:47 +0530

    Data Types

---
+++ Firnas/src/Basics/DataTypes.java
@@ -0,0 +1,30 @@
+package Basics;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		System.out.println("----Data Types---- ");
+		/*
+		 * 1.Numbers
+		 * 2.Floating point numbers
+		 * 3.Character
+		 * 4.Boolean 
+		 * 
+		 */
+		System.out.println("Basic Datatypes");
+		System.out.println("1.Numbers\n"
+				+ "2.Floating point numbers\n"
+				+ "3.Character\n"
+				+ "4.Boolean ");
+		// \n --refers to new line
+		
+		//Numbers - (byte, short, int, long)
+		byte b = 100;
+		short s = 32000;
+		int i = 2147483647;
+		long l = 9223372036854775807L;
+		
+		
+		
+		//
+	}
+}
