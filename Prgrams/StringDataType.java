713837118b7bcf04ba5207ede02d255725ab28cb
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2022-02-08 11:18:13 +0530

    Add files via upload

---
+++ Prgrams/StringDataType.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class StringDataType {
+	public static void main(String[] args) {
+		//Type - 1 
+		String name = "Musharaf Aahel";
+		
+		//Type - 2
+		String degree = new String("B.Sc Computer Science");
+		
+		//Type - 3
+		String college = String.valueOf("The New College");
+		
+		System.out.println("I am "+name+" studying "+degree+" at "+college);
+	}
+}	
