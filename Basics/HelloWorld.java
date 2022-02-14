af90879883a34cd8b69ec04a21ab055fb262ff4b
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2022-02-14 09:25:45 +0530

    Update HelloWorld.java

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -3,7 +3,7 @@ package Basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		//Output Statement
-		System.out.println("Hello World!");
+		System.out.println("Hello  World!");
 		// Type "main" and press "ctrl + space bar"
 		// Type "sysout" and press "ctrl + space bar"
 	}
