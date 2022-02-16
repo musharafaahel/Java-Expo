6fc93ea0046be874491c8f52f05505b21a016fb1
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2022-02-16 21:05:56 +0530

    Add files via upload

---
+++ Basics/Complete Reference/Chapter 3/CharDemo2.java
@@ -0,0 +1,13 @@
+// char variables behave like integers
+
+class CharDemo2 {
+	public static void main(String args[]){
+		char ch1;
+		
+		ch1 = 'x';
+		System.out.println("ch1 contains "+ch1);
+		
+		ch1++;
+		System.out.println("ch1 is now "+ch1);
+	}
+}
