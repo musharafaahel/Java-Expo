a738b9797d7b43885ea21426b4a3f076f9a44cb5
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2022-02-16 10:30:25 +0530

    Update Area.java

--- Basics/Complete Reference/Chapter 3/Area.java
+++ Basics/Complete Reference/Chapter 3/Area.java
@@ -3,8 +3,8 @@
 class Area {
 	public static void main(String args[]){
 		double pi, r, a;
-		r = 10.8; //radius of the circle
-		pi =3.1416; //pi, approximately
+		r = 10.8;       //radius of the circle
+		pi =3.1416;     //pi, approximately
 		a = pi * r * r; // Compute area
 		
 		System.out.println("Area of Circle is  "+a);
