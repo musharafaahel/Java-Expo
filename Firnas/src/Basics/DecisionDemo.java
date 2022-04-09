2eef7e767018057a43297f84adda86c4f1f0730f
musharafaahel <musaahel@gmail.com>
2022-04-09 14:07:42 +0530

    Testing

---
+++ Firnas/src/Basics/DecisionDemo.java
@@ -0,0 +1,50 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class DecisionDemo {
+	public static void main(String[] args) {
+		boolean result=false;
+		if(result) {
+			System.out.println("yes done....................");
+		}
+		else {
+			System.out.println("yes not done............");
+		}
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your day......:");
+		String day=scan.next();
+		
+		
+		if(day.equals("monday")) {
+			System.out.println("solomon grandy born on monday...");
+		}
+		else if(day.equals("tuesday")) {
+			System.out.println("married on tuesday.....");
+		}
+		else if(day.equals("wenesday")) {
+			System.out.println("died on wednesday........");
+		}
+		else {
+			System.out.println("bla bla bla bla.........");
+		}
+		
+		System.out.println("Enter a number....:");
+		int n=scan.nextInt();
+		
+		if(n%2==0) {
+			System.out.println("Even number............");
+		}
+		else {
+			System.out.println("odd number............");
+		}
+		
+		//? - ternary operator
+		String s=(n%2!=0)?"Even number...":"Odd number...";
+		System.out.println(s);
+		
+		scan.close();
+		
+	}
+}
